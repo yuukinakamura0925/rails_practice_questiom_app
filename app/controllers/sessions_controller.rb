5fc9db30fc1caf145848305e334f2c9cbec8fc97
yuukinakamura0925 <ynakamara04@gmail.com>
2021-05-11 20:08:49 +0900

    Add: login ログイン機能

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "users/new"
   resources :users
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 end
